694ad5faceccc5e1505763ec334bb94affe92352
kjoedicker <kjoedicker@gmail.com>
2023-05-26 11:24:57 -0400

    feat(main): leverage `tui` and `keyboard` to listen for keyboard input and display it accordingly. Ultimately sending a final selection to `stdout`
    Example usage: `echo "Choice 1\nChoice 2\nChoice 3" | cargo run > some-file.txt`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+mod keyboard;
+mod tui;
+
+use keyboard::KeyboardState;
+use std::{io, process};
+
+fn get_stdin() -> Vec<String> {
+    let stdin = io::stdin();
+
+    stdin.lines().map(|line| line.unwrap()).collect()
+}
+
+fn start() {
+    tui::spawn();
+
+    // Dependents
+    let selections = get_stdin();
+
+    let mut keyboard_handler = keyboard::init_keyboard_handler(&selections);
+
+    loop {
+        match keyboard_handler.keyboard_state {
+            KeyboardState::Exit => {
+                tui::clear_screen();
+                process::exit(0);
+            }
+            KeyboardState::FinalSelection => {
+                tui::exit();
+
+                let selection_index = keyboard_handler.highlighted_selection.clone() as usize;
+                let final_selection = selections.get(selection_index).unwrap();
+                print!("{}", final_selection);
+                break;
+            }
+            KeyboardState::Input => {
+                tui::clear_screen();
+                tui::display_selections(&selections, keyboard_handler.highlighted_selection);
+                
+                // TODO: revisit how we want to delimit
+                let delimiter = "============".to_owned(); 
+                tui::display_input(&delimiter);
+                // TODO: add some sort of input matching that ties into the selection highlight
+                tui::display_input(&keyboard_handler.input_buffer);
+            }
+            // TODO: handle me
+            _ => {}
+        }
+
+        keyboard_handler.listen();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    start();
 }
